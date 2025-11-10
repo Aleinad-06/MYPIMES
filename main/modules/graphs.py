4bc2e5734482957f2adf8dc5a43f1f906af183de
Aleinad-06 <danieladiazalvarez06@gmail.com>
2025-11-09 22:34:49 -0500

    actualizacion

--- main/modules/graphs.py
+++ main/modules/graphs.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
+from collections import Counter
 
 def  promedio(num: list):
     return sum(num) / len(num)
 
+def porciento(parte, total):
+    return parte/total
 
+# def mar_comercializan(marcas):
+#     marcasss = []
+#     for marc in marcas:
+#         if marcas[marc] == "":
         
-    
+
